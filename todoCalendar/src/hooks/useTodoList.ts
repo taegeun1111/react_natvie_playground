7e4802b9ddf7914de9bd1cbafbf4f47b08e2daa5
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2024-10-13 22:07:58 +0900

    FEAT: useTodoList hook

---
+++ todoCalendar/src/hooks/useTodoList.ts
@@ -0,0 +1,53 @@
+import dayjs from "dayjs";
+import { useState } from "react";
+
+interface ITodoItem {
+  id: number;
+  content: string;
+  date: dayjs.Dayjs;
+  isSuccess: boolean;
+}
+
+const defaultTodoList: ITodoItem[] = [
+  { id: 1, content: "1", date: dayjs(), isSuccess: true },
+  { id: 2, content: "2", date: dayjs(), isSuccess: false },
+  { id: 3, content: "3", date: dayjs(), isSuccess: true },
+];
+
+export const useTodoList = (selectedDate: dayjs.Dayjs) => {
+  const [todoList, setTodoList] = useState(defaultTodoList);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    const newTodoList = [
+      ...todoList,
+      {
+        id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+        content: input,
+        date: selectedDate,
+        isSuccess: false,
+      },
+    ];
+
+    setTodoList(newTodoList);
+  };
+
+  const removeTodo = (todoId: number) => {
+    const newTodoList = todoList.filter((list) => list.id !== todoId);
+    setTodoList(newTodoList);
+  };
+
+  const toggleTodo = (todoId: number) => {
+    const newTodoList = todoList.map((todo) => {
+      if (todo.id !== todoId) return todo;
+      return {
+        ...todo,
+        isSuccess: !todo.isSuccess,
+      };
+    });
+
+    setTodoList(newTodoList);
+  };
+
+  return {};
+};
